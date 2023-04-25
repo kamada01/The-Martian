451fa9aa56e9ae3ec18e199a702fde294cf72d9a
Ek-kE-LL <78842707+Ek-kE-LL@users.noreply.github.com>
2023-04-25 19:39:33 +0800

    BrainMole Related files
    Edward 4/25 1939:
    updated the script and animation

--- Assets/BrainMole/BrainMoleMovement.cs
+++ Assets/BrainMole/BrainMoleMovement.cs
@@ -5,27 +5,30 @@ using UnityEngine;
 public class BrainMoleMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
-    [SerializeField] private float range;
+    [SerializeField] private float attackRange;
     [SerializeField] private float attackCD;
     [SerializeField] private int damage;
     [SerializeField] private CapsuleCollider2D capsuleCollider;
+    [SerializeField] private float vision;
+    [SerializeField] private Transform player;
 
+    private Vector2 DirectionToPlayer;
 
     private float cdTimer = Mathf.Infinity;
     private float HP;
 
     Animator animator;
     private Rigidbody2D rb;
-    private BrainMoleVisionController visionController;
-    private Vector2 targetDirection;
+    //private BrainMoleVisionController visionController;
+    //private Vector2 targetDirection;
     private RaycastHit2D hit;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
-        visionController = GetComponent<BrainMoleVisionController>();
-        HP = 2;    
+        //visionController = GetComponent<BrainMoleVisionController>();
+        HP = 1;    
 
     }
 
@@ -38,16 +41,10 @@ public class BrainMoleMovement : MonoBehaviour
             if (cdTimer >= attackCD)
             {
                 cdTimer = 0;
-                Debug.Log("Launching attack");
-
+                //Debug.Log("Launching attack");
                 animator.SetTrigger("attack");
-                if (hit.collider != null && hit.collider.CompareTag("Player"))
-                {
-                    //Reduce Player's HP in a sub
 
-                    //debug message
-                    //Debug.Log("Attack causes damage");
-                }
+                StartCoroutine(AttackPlayer());
             }
         }
         else
@@ -56,16 +53,31 @@ public class BrainMoleMovement : MonoBehaviour
         }
     }
 
+    private IEnumerator AttackPlayer()
+    {
+        // Wait for 0.5 second 
+        yield return new WaitForSeconds(0.5f);
+        // only reduce player's HP if the player is still within the attacking range after 1 sec
+        if (hit.collider != null && hit.collider.CompareTag("Player"))
+        {
+            //Reduce Player's HP in a sub
+            //DamagePlayer(damage);
+            //debug message
+            //Debug.Log("Attack causes damage");
+        }
+    }
+
+
     private bool PlayerWithinAttackRange()
     {
         //create a box collider used for attack detection
         hit = Physics2D.BoxCast(capsuleCollider.bounds.center + 
-            transform.right * range * transform.localScale.x,
+            transform.right * attackRange * transform.localScale.x,
             capsuleCollider.bounds.size*0.5f, 0, Vector2.left, 0);
 
         if (hit.collider != null && hit.collider.CompareTag("Player"))
         {
-            Debug.Log("Player within attack range.");
+            //Debug.Log("Player within attack range.");
             return true;
         }
         else
@@ -79,7 +91,7 @@ public class BrainMoleMovement : MonoBehaviour
         //draw the boxcollider representing the attacking range 
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(capsuleCollider.bounds.center +
-            transform.right * range * transform.localScale.x,
+            transform.right * attackRange * transform.localScale.x,
             capsuleCollider.bounds.size* 0.5f);
     }
 
@@ -88,43 +100,59 @@ public class BrainMoleMovement : MonoBehaviour
         //To be implement after setting up the player's hp codes
     }
 
-    private void TakingDamage()
+    public void TakingDamage(float damageTaken)
     {
         //To be implement after settig up the player's weapons
+        HP = HP - damageTaken;
+
+        if (HP <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
-    private void MovingTowardsPlayer() { 
-        if (visionController.AwareOfPlayer)
+    private void MovingTowardsPlayer() {
+        if (AwareOfPlayer())
         {
-            targetDirection = visionController.DirectionToPlayer;
             animator.SetTrigger("move");
-            Debug.Log("Player detected. Moving towards the player");
+            //Debug.Log("Player detected. Moving towards the player");
 
             //Flip the spirte if necessary
-            if (targetDirection.x > 0)
+            if (DirectionToPlayer.x > 0)
             {
                 transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
             }
-            else if (targetDirection.x < 0)
+            else if (DirectionToPlayer.x < 0)
             {
                 transform.localScale = new Vector3(-1 * Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
             }
 
-            rb.AddForce(targetDirection * speed * Time.deltaTime);
+            rb.AddForce(DirectionToPlayer * speed * Time.deltaTime);
 
         }
         else
         {
-            targetDirection = Vector2.zero;
+            DirectionToPlayer = Vector2.zero;
             rb.velocity = Vector2.zero;
             animator.SetTrigger("idle");
-            Debug.Log("Player Not detected.");
+            //Debug.Log("Player Not detected.");
         }
 
     }
 
-
-
+    private bool AwareOfPlayer()
+    {   //check if the player is within the "detection range"
+        Vector2 enemyToPlayerVector = player.position - transform.position;
+        if (enemyToPlayerVector.magnitude <= vision)
+        {
+            DirectionToPlayer = enemyToPlayerVector.normalized;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 
 
 }
