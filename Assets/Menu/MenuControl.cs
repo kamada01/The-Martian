a183ce3742d92a4ea2348668e6ab14392622eb83
kamada01 <ada.kamym@gmail.com>
2023-04-29 15:28:00 +0800

    Merge menucontrol changes

--- Assets/Menu/MenuControl.cs
+++ Assets/Menu/MenuControl.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,6 +17,45 @@ public class MenuControl : MonoBehaviour
         SceneManager.LoadScene("SampleScene_1");
         player = (Astronaut)FindAnyObjectByType(typeof(Astronaut));
         Debug.Log(player.CurHealth);
+    }
+
+    public void LoadGameYes()
+    {
+        if (PlayerPrefs.HasKey("SavedGame"))
+        {
+            GameToLoad = PlayerPrefs.GetString("SavedGame");
+            SceneManager.LoadScene(GameToLoad);
+        }
+        else
+        {
+            NoSaveGame.SetActive(true);
+        }
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
+=======
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour
+{
+    [Header("Games to Load")]
+    private string GameToLoad;
+    [SerializeField] private GameObject NoSaveGame = null;
+    public Astronaut player;
+
+    public void NewGameYes()
+    {
+        SceneManager.LoadScene("SampleScene_1");
+        player = (Astronaut)FindAnyObjectByType(typeof(Astronaut));
+        Debug.Log(Astronaut.CurHealth);
     }
 
     public void LoadCutScene()
@@ -44,3 +84,4 @@ public class MenuControl : MonoBehaviour
         Application.Quit();
     }
 }
+>>>>>>> 98acc5bb4c9fbbc1d81044083345984aaa6b1e58
